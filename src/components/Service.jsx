ee77bedbd86e6b4a9e89a1cc37e3546b07ec95d4
Aju Shabin <ajushabin@Ajus-MacBook-Pro.local>
2025-03-30 02:59:11 +0530

    Updated background animation and fixed styles

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -7,6 +7,10 @@ import Image2 from "../images/service/2.png";
 import Image3 from "../images/service/3.png";
 import Image4 from "../images/service/4.png";
 import Image5 from "../images/service/5.png";
+import Imagebr from "../images/service/br.jpeg";
+import Image6 from "../images/service/6.jpeg";
+
+
 const Service = () => {
   const navigate = useNavigate();
 
@@ -43,7 +47,26 @@ const Service = () => {
 
         {/* Personal Information */}
         <div className="col-md-8">
-          <h2 className="service-history-heading">Fr. Anish</h2>
+          <h2 className="service-history-heading">Fr. Alphonse</h2>
+          <p><strong>Priest ID:</strong> RCDK149</p>
+          <p><strong>Date of Birth:</strong> 03.07.1994</p>
+          <p><strong>Place of Birth:</strong> Kamplar</p>
+          <p><strong>Date of Ordination:</strong> 14.04.2024</p>
+          <p><strong>Ministering At:</strong> Bhagalpur diocese </p>
+          <p><strong>Address:</strong> Church of Our Lady of Assumption, Mathiravilai</p>
+          <p><strong>Email:</strong> anishkuz94@gmail.com</p>
+        </div>
+      </div>
+      {/* Images-5 */}
+      <div className="row align-items-center my-5">
+        {/* Profile Image */}
+        <div className="col-md-4 text-center">
+          <img src={Imagebr} alt="Profile" className="img-fluid profile-image" />
+        </div>
+
+        {/* Personal Information */}
+        <div className="col-md-8">
+          <h2 className="service-history-heading">Br Maria Simson</h2>
           <p><strong>Priest ID:</strong> RCDK149</p>
           <p><strong>Date of Birth:</strong> 03.07.1994</p>
           <p><strong>Place of Birth:</strong> Kamplar</p>
@@ -59,10 +82,10 @@ const Service = () => {
         <div className="col-md-4 text-center">
           <img src={Image3} alt="Profile" className="img-fluid profile-image" />
         </div>
-
+      
         {/* Personal Information */}
         <div className="col-md-8">
-          <h2 className="service-history-heading">Fr. Anish</h2>
+          <h2 className="service-history-heading">Sr Ezhil</h2>
           <p><strong>Priest ID:</strong> RCDK149</p>
           <p><strong>Date of Birth:</strong> 03.07.1994</p>
           <p><strong>Place of Birth:</strong> Kamplar</p>
@@ -81,7 +104,7 @@ const Service = () => {
 
         {/* Personal Information */}
         <div className="col-md-8">
-          <h2 className="service-history-heading">Fr. Anish</h2>
+          <h2 className="service-history-heading">Sr Crystal shyla</h2>
           <p><strong>Priest ID:</strong> RCDK149</p>
           <p><strong>Date of Birth:</strong> 03.07.1994</p>
           <p><strong>Place of Birth:</strong> Kamplar</p>
@@ -110,6 +133,26 @@ const Service = () => {
           <p><strong>Email:</strong> anishkuz94@gmail.com</p>
         </div>
       </div>
+          {/* Images-5 */}
+          <div className="row align-items-center my-5">
+        {/* Profile Image */}
+        <div className="col-md-4 text-center">
+          <img src={Image6} alt="Profile" className="img-fluid profile-image" />
+        </div>
+
+        {/* Personal Information */}
+        <div className="col-md-8">
+          <h2 className="service-history-heading">Sr Armella S R A</h2>
+          <p><strong>Priest ID:</strong> RCDK149</p>
+          <p><strong>Date of Birth:</strong> 03.07.1994</p>
+          <p><strong>Place of Birth:</strong> Kamplar</p>
+          <p><strong>Date of Ordination:</strong> 14.04.2024</p>
+          <p><strong>Ministering At:</strong> Co-Pastor, Mathiravilai</p>
+          <p><strong>Address:</strong> Church of Our Lady of Assumption, Mathiravilai</p>
+          <p><strong>Email:</strong> anishkuz94@gmail.com</p>
+        </div>
+      </div>
+      
     </div>
   );
 };
